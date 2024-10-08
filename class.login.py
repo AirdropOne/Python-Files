56b770c88696e2e79c42429e471a25cdd7bd447d
Max <max.sevier.smart@gmail.com>
2024-10-08 23:22:51 +0100

    08.10

--- class.login.py
+++ class.login.py
@@ -2,11 +2,11 @@ import tkinter
 import mysql.connector
 import sqlite3
 from tkinter import messagebox
+import bcrypt                       ## Auto handles salting, widely recomended - research and reference
 
 
 
-
-
+## Object oriented approach, greater efficiency, more scalable - maybe make in C++ for better scalability as future concept since its better at that?
 
 
 class   Login_Page():
@@ -51,6 +51,8 @@ class   Login_Page():
         self.create_account_button=tkinter.Button(self.login_window, text="Create Account", command=self.open_registration_window)                ## Assigning a name and adding text to create accountbutton       and      initiate page traversal??
         self.create_account_button.grid(row=4, column=1, pady=5)                                                        ## Placing the button in the grid
 
+    def verify_password(self, password, stored_password):                                           ##checks the password input, hashes it with crype then compares to the stored password on the database (defined the login function)
+        return bcrypt.checkpw(password.encode('utf-8'), stored_password)
 
     def login(self):
         email = self.email_entry.get()                                                                  ## assigning a name to the function of pulling the values that have been input into the email entry field
@@ -59,13 +61,18 @@ class   Login_Page():
         connect = sqlite3.connect("practise_DB.db")                                                 ## assigning a name to the funtion of connecting to my practise datatbase         and       remember the .db
         c = connect.cursor()                                                                        ## assigning a name to the function of making a request to the connected database
 
-        c.execute("SELECT * FROM Login_Database WHERE Email=? AND Password=?", (email, password))  ## "?" work as a placeholder for the tuple values
+        c.execute("SELECT Password FROM Login_Database WHERE Email=?", (email,))                ## "?" work as a placeholder for the tuple values- include the comma since its a tuple. Pulling the stored password to compare against hashed password from field entry
     
-        row=c.fetchone()                                                                            ## goes one by one along a row
-        if row:
-            messagebox.showinfo("Info", "Login Success")
+        row=c.fetchone()                                                                            ## goes one by one along a row looking for matching email
+        if row:                                                                                 ## if a matching email was found
+            stored_password = row[0]                                                            ## pulls the stored password from the database that correlates to the matching email
+            if self.verify_password(password, stored_password):                                 ## if verify password passes its check, login success
+                messagebox.showinfo("Info", "Login Success")
+            else: 
+                messagebox.showinfo("Info", "Incorrect Password")                               ## Else the password didnt correlate to the matching emails
+            
         else:
-            messagebox.showinfo("Info", "Login Failed")
+            messagebox.showinfo("Info", "Login Failed")                                         ## Else there wasnt a stored email matching the one put in the field
 
 
     def open_registration_window(self):
@@ -103,27 +110,47 @@ class Registration_Page():
 
 
         self.register_button=tkinter.Button(self.reg_window, text="Register", command=self.register)
-        self.register_button.grid(row=4, column=1, pady=5)
+        self.register_button.grid(row=3, column=1, pady=5)
+
+
+        self.back_to_login_button=tkinter.Button(self.reg_window, text="Back to Login", command=self.back_to_login)               ## button for returning to login page and destroying instance of registration page
+        self.back_to_login_button.grid(row=4, column=1, pady=5)
+
+
+
+    def hash_password(self, password):                                          ## Uses bcrypt to hash the password input
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())        ## takes the password and encodes it using 'utf-8' to turn the string into bytes that bcrypy can encode, then salts for added security against rainbow tables. (research and reference) why use utf and bcrypt?
 
     def register(self):
         email = self.email_reg.get()
         password = self.password_reg.get() 
+        hashed_password = self.hash_password(password)
+
 
         connect = sqlite3.connect("practise_DB.db")
         c = connect.cursor()
 
         c.execute("SELECT * FROM Login_Database WHERE Email=?" , (email,))                ## check if email already exists in database ## When using placeholders (?), the second argument must always be a tuple, even if it's a single value. add comma
-        row = c.fetchone()
+        row = c.fetchone()                                                                ##
 
         if row:
-            messagebox.showinfo("Error", "Email already exists")
+            messagebox.showinfo("Error", "Email already exists")                          ## If email was fetched, it already exists and returns this message
         else:
-            c.execute("INSERT INTO Login_Database (Email, Password) VALUES (?, ?)", (email, password))     ## Establish the touple paired to the placeholder outside of the sql query
-            connect.commit()
+
+
+            c.execute("INSERT INTO Login_Database (Email, Password) VALUES (?, ?)", (email, hashed_password))     ## Establish the touple paired to the placeholder outside of the sql query
+            connect.commit()                                                                    ## Commits the change to the database
             messagebox.showinfo("Info", "Account Created")
+
+    def back_to_login(self):                    
+
+        self.reg_window.destroy()           ## destroy registration instance
+                
 
 
     
+
+
 
 
 if __name__ == "__main__":
@@ -134,4 +161,3 @@ if __name__ == "__main__":
 
 
 ## Need to add a back function from the register page to the login page
-## Need to hash passwords
